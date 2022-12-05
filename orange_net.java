386c551190a2e48264a928e73cb919d259505673
Shrouk Ashraf <111459677+Shrouk89@users.noreply.github.com>
2022-12-05 15:26:34 +0200

    Create orange_net.java

---
+++ orange_net.java
@@ -0,0 +1,15 @@
+package os;
+
+public class orange_net implements service_provider {
+	public String provider_name="orange internet payment";
+	@Override
+	public String getproviderName() {
+		// TODO Auto-generated method stub
+		return provider_name;
+	}
+	public boolean aggrement(int telephone, int amount) {
+		
+		return true;
+	}
+
+}
