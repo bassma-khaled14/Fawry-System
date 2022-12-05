90f24a58d5c6ab5eea4d1a60e83a81c67e0d1c86
Shrouk Ashraf <111459677+Shrouk89@users.noreply.github.com>
2022-12-05 21:41:47 +0200

    Create transactions.java

---
+++ transactions.java
@@ -0,0 +1,39 @@
+package jvm;
+
+
+	import java.util.*;
+	import java.util.Vector;
+
+	public class transactions {
+		public int size=100000;
+		Vector <String> vc =new Vector<>();
+	  String []payment=new String [size];
+	  String[]addWallet=new String [size];
+	  String[]refund=new String [size];
+	  public void print() {
+		  for(int i=0;i<vc.size();i++){
+			  System.out.println(vc.elementAt(i));
+		  }
+	  }
+	  public  void listpayment(int amount) {
+		 
+		  //String f = Arrays.toString(payment);  //Array in string format
+	     // System.out.println(f);
+	      String am=String.valueOf(amount);
+	     String fina="Payment : "+am;
+	      vc.add (fina);
+	     System.out.println("Payment : "+am);
+	  }
+	 
+	  public void listrefund(int amount) {
+
+		  String m = Arrays.toString(refund);  //Array in string format
+	      System.out.println(m);
+	      String am=String.valueOf(amount);
+	      vc.add("refund : "+am);
+	  }
+	  
+	  
+	}
+
+
