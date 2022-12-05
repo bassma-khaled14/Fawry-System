7d564b081a458bd5130931091e54df7c7e49199b
Bassma Khaled <112968009+bassma-khaled14@users.noreply.github.com>
2022-12-05 15:45:16 +0200

    Create quarter_land.java

---
+++ quarter_land.java
@@ -0,0 +1,15 @@
+package jmv;
+
+public class quarter_land implements service_provider {
+	public String provider_name="quarter landline";
+	@Override
+	public String getproviderName() {
+		// TODO Auto-generated method stub
+		return provider_name;
+	}
+	public boolean aggrement(int telephone, int amount) {
+		
+		return true;
+	}
+
+}
