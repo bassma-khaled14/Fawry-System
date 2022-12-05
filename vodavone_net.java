ddecba8ced14d627ec2f285708767d77812ddbb9
Shrouk Ashraf <111459677+Shrouk89@users.noreply.github.com>
2022-12-05 15:26:32 +0200

    Create vodavone_net.java

---
+++ vodavone_net.java
@@ -0,0 +1,15 @@
+package os;
+
+public class vodavone_net implements service_provider {
+	public String provider_name="vodavone internet payment";
+	@Override
+	public String getproviderName() {
+		// TODO Auto-generated method stub
+		return provider_name;
+	}
+	public boolean aggrement(int telephone, int amount) {
+		
+		return true;
+	}
+
+}
