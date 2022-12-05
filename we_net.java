688eeea7b185675310b65f711176a54ce603879d
Shrouk Ashraf <111459677+Shrouk89@users.noreply.github.com>
2022-12-05 15:26:29 +0200

    Create we_net.java

---
+++ we_net.java
@@ -0,0 +1,15 @@
+package os;
+
+public class we_net implements service_provider {
+	public String provider_name="we internet payment";
+	@Override
+	public String getproviderName() {
+		// TODO Auto-generated method stub
+		return provider_name;
+	}
+	public boolean aggrement(int telephone, int amount) {
+		
+		return true;
+	}
+
+}
