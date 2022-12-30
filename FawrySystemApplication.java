70fecdd406ad80e66748e61f503bd09a32733468
Shrouk Ashraf <111459677+Shrouk89@users.noreply.github.com>
2022-12-30 17:22:49 +0200

    Create FawrySystemApplication.java

---
+++ FawrySystemApplication.java
@@ -0,0 +1,13 @@
+package com.fci.advanced.se.FawrySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FawrySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FawrySystemApplication.class, args);
+	}
+
+}
