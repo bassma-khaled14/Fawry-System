05d75d5cc98903306f2e852f2c490cb241b8df9a
Bassma Khaled <bassmakhaleed123@gmail.com>
2022-12-30 23:48:21 +0200

    Update user.java

--- user.java
+++ user.java
@@ -1,115 +1,67 @@
-
-package os;
+package com.fci.advanced.se.FawrySystem;
 import java.util.ArrayList;
 public class user {
-public String name;
-public String mail;
-public int pass;
-public double NewUserDiscount = 0.1;
-public double specificDiscount;
-//public state = true;//not occupied
-Iservice service;
-Ipayment paymethod;
-public wallet wallet = new wallet();
-public user()
-{
-	
-}
-public user(String name,String mail,int pass)
-{
-	this.name=name;
-	this.mail=mail;
-	this.pass=pass;
-}
-public boolean signup(ArrayList<user> a,String name,String mail,int pass) {
-	int x=0;
-	for(int i=0; i<a.size();i++) {
-		if(a.get(i).getName().equals(name) || a.get(i).getMail().equals(mail))//ahmed-ahmed2
-			x++;
-	}
-	if(x==0)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    public String name;
+    public String mail;
+    public Integer pass;
+    public double NewUserDiscount = 0.1;
+    public double specificDiscount;
+    public static ArrayList<user> userentity=new ArrayList<user>();
 
-	
-}
-public boolean login(ArrayList<user> a,String mail, int pass) {
-	int x=0;
-	for(int i=0; i<a.size(); i++)
-	{
-		if(a.get(i).getMail().equals(mail) && a.get(i).getPassword() == pass)
-		{
-			x++;
-			break;
-		}
-	}
-	if(x>0)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-	
-}
+    public com.fci.advanced.se.FawrySystem.wallet wallet = new wallet();
+    public user()
+    {
+        userentity.add(new user("bassma","bassma@gmail",123));
+    }
+    public user(String name, String mail, Integer pass)
+    {
+        this.name=name;
+        this.mail=mail;
+        this.pass=pass;
+    }
+    public ArrayList<user> getUserEntity()
+    {
+       return userentity;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public void setMail(String mail)
+    {
+        this.mail=mail;
+    }
+    public void setpass(int pass)
+    {
+        this.pass=pass;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getMail()
+    {
+        return mail;
+    }
+    public int getpass()
+    {
+        return pass;
+    }
+    public void setUserentity(String name,String mail,int pass)
+    {
+        userentity.add(new user(name,mail,pass));
+    }
+    public void setDiscount(double discount)
+    {
+        specificDiscount = discount;
+    }
+    public double getDiscount()
+    {
+        return specificDiscount;
+    }
+    public void setWalletBalance(int balance)
+    {
 
-public Iservice search(String servName) {
-	if(servName.equals(service.name)) {
-		return service;
-	}
-	else {
-		return null;
-	}
-	//main > Iservice.search(string servName)
-}
-public int[] reqrefund(int trans_num,int serv_id){
-	
-  int arr[]=new int[2];
-  arr[0]=trans_num;
-  arr[1]=serv_id;
-  return arr;
-  
-}
-public void setMail(String mail)
-{
-	this.mail = mail;
-}
-public void setName(String name)
-{
-	this.name = name;
-}
-public void setPassword(int pass)
-{
-	this.pass = pass;
-}
-public int getPassword()
-{
-	return pass;
-}
-public String getMail()
-{
-	return mail;
-}
-public String getName()
-{
-	return name;
-}
-public void setDiscount(double discount)
-{
-	specificDiscount = discount;
-}
-public double getDiscount()
-{
-	return specificDiscount;
-}
-public void setWalletBalance(int balance)
-{
-	
-}
+    }
+
 }
